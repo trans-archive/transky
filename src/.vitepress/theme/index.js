0c04e3636e639cdb085184ec49200f56fc30e534
command_block <mtf@ik.me>
2024-01-19 14:18:43 +0800

    feat: 添加图片查看器

--- src/.vitepress/theme/index.js
+++ src/.vitepress/theme/index.js
@@ -1,4 +1,12 @@
+import { useRoute } from "vitepress";
 import DefaultTheme from "vitepress/theme";
+import imageViewer from "vitepress-plugin-image-viewer";
+import "viewerjs/dist/viewer.min.css";
 import "./indent.css";
 
-export default DefaultTheme;
+export default {
+  ...DefaultTheme,
+  setup() {
+    imageViewer(useRoute());
+  },
+};
