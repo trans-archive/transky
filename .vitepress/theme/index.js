a70ca8ec1b202c588b38639a7ea6fd1795196799
command_block <mtf@ik.me>
2024-04-13 14:59:55 +0800

    chore: remove giscus

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,6 +1,5 @@
-import { useData, useRoute } from "vitepress";
+import { useRoute } from "vitepress";
 import DefaultTheme from "vitepress/theme";
-import giscusTalk from "vitepress-plugin-comment-with-giscus";
 import imageViewer from "vitepress-plugin-image-viewer";
 import "viewerjs/dist/viewer.min.css";
 import "./indent.css";
@@ -8,29 +7,6 @@ import "./indent.css";
 export default {
   ...DefaultTheme,
   setup() {
-    const { frontmatter } = useData();
-    const route = useRoute();
-    imageViewer(route);
-    giscusTalk(
-      {
-        repo: "proskynova/proskynova.github.io",
-        repoId: "R_kgDOLEtJ4A",
-        category: "评论区",
-        categoryId: "DIC_kwDOLEtJ4M4Cch85",
-        mapping: "title",
-        strict: 1,
-        reactionsEnabled: 1,
-        emitaMetadata: 1,
-        inputPosition: "top",
-        theme: "preferred_color_scheme",
-        lang: "zh-CN",
-        loading: "lazy",
-      },
-      {
-        frontmatter,
-        route,
-      },
-      true,
-    );
+    imageViewer(useRoute());
   },
 };
