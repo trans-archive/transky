4e00a75592498af14e9839d1b1da47e4016be343
Cryolitia PukNgae <Cryolitia@gmail.com>
2024-09-26 21:23:07 +0800

    rfct: migrate to vitepress-theme-project-trans

---
+++ src/.vitepress/env.d.ts
@@ -0,0 +1,13 @@
+declare module 'markdown-it-pangu' {
+  import type { PluginSimple } from 'markdown-it'
+
+  const pangu: PluginSimple
+  export default pangu
+}
+
+declare module 'markdown-it-katex' {
+  import type { PluginSimple } from 'markdown-it'
+
+  const katex: PluginSimple
+  export default katex
+}
---
+++ src/.vitepress/theme/CopyrightInfo.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="tip custom-block">
+    <p class="custom-block-title">Copyright</p>
+    <p>
+      <span>本作品基于</span><a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh-hans">CC BY-NC-SA
+        4.0</a><span>许可发布。</span>
+    </p>
+    <p> <span>版权所有 © 2016-2024</span><a href="https://www.weibo.com/p/1005055513855401">乱世银娘</a><span>、</span><a
+        href="https://github.com/transky-book/transky/graphs/contributors">Transky 全体贡献者</a></p>
+  </div>
+</template>
